52606714c119a8bb1ed46c9a700876222c8df864
Cupkeka <andradebrandon26@gmail.com>
2024-07-29 09:14:19 -0500

    cambio de fondo login, y cambios en entrenador

--- Public/login.php
+++ Public/login.php
@@ -75,6 +75,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
     <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+    <style>
+        body.bg-primary {
+            background-image: url('../Assets/img/looney.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+    </style>
 </head>
 
 <body class="bg-primary">
