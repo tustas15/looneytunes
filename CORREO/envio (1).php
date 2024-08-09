fc764a1316ce2db8dadc52d4f0f4af934661d60b
Carlos Rosales <tustasgamer@gmail.com>
2024-08-08 20:49:49 -0500

    correo

---
+++ CORREO/envio (1).php
@@ -0,0 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//require_once "PHPMailer-6.9.1/vendor/autoload.php";
+// Activar o desactivar excepciones mediante variable
+
+require 'PHPMailer-6.9.1/src/Exception.php';
+require 'PHPMailer-6.9.1/src/PHPMailer.php';
+require 'PHPMailer-6.9.1/src/SMTP.php';
+
+
+$debug = true;
+try {
+    // Crear instancia de la clase PHPMailer
+    $mail = new PHPMailer($debug);
+    if ($debug) {
+        // Genera un registro detallado
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+    }
+    // Autentificación con SMTP
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    // Login
+    $mail->Host = "mail.pensamientosocial.com";
+    $mail->Port = 587;
+    $mail->Username = "cuarto@pensamientosocial.com";
+    $mail->Password = "cuarto*itsi123";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->setFrom('cuarto@pensamientosocial.com', 'Cuarto');
+    $mail->addAddress('eicono@hotmail.com', 'EICONO');
+    //$mail->addAttachment("/home/user/Escritorio/imagendeejemplo.png", "imagendeejemplo.png");
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+    $mail->isHTML(true);
+    $mail->Subject = 'Saludo cuarto software';
+    $mail->Body = 'Hola a todos los de cuarto software, como estan';
+    $mail->AltBody = 'Texto como elemento de texto simple';
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: ".$e->getMessage();
+}
