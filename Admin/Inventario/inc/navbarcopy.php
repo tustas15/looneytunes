e4ec8d86846ec12c1b73223d5f1c30ab679d0f46
Carlos Rosales <tustasgamer@gmail.com>
2024-08-11 21:43:56 -0500

    cambios

--- Admin/Inventario/inc/navbarcopy.php
+++ Admin/Inventario/inc/navbarcopy.php
@@ -1,5 +1,4 @@
 <body class="nav-fixed">
-
     <nav class="topnav navbar navbar-expand shadow justify-content-between justify-content-sm-start navbar-light bg-white" id="sidenavAccordion">
         <!-- Sidenav Toggle Button-->
         <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 me-2 ms-lg-2 me-lg-0" id="sidebarToggle"><i data-feather="menu"></i></button>
