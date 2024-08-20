ae7f9c87735cc311cb350b6aa26c472a63479544
Cupkeka <andradebrandon26@gmail.com>
2024-08-20 17:56:20 -0500

    Cambio de rutas

--- Entrenador/guardar_detalles.php
+++ Entrenador/guardar_detalles.php
@@ -1,5 +1,5 @@
 <?php
-require_once('/xampp/htdocs/looneytunes/admin/configuracion/conexion.php');
+require_once('../admin/configuracion/conexion.php');
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
