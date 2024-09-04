fa53d4febc7cb7299e4e16d88f5e3694028a477c
Carlos Rosales <tustasgamer@gmail.com>
2024-09-03 21:46:17 -0500

    "Updated navbar brand text from 'Dashboard' to 'CASF' in three files: revisar_categorias.php, header.php, and navbarcopy.html."

--- Admin/Categorias/revisar_categorias.php
+++ Admin/Categorias/revisar_categorias.php
@@ -268,7 +268,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <!-- * * Tip * * You can use text or an image for your navbar brand.-->
         <!-- * * * * * * When using an image, we recommend the SVG format.-->
         <!-- * * * * * * Dimensions: Maximum height: 32px, maximum width: 240px-->
-        <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="/looneytunes/admin/indexad.php">Dashboard</a>
+        <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="/looneytunes/admin/indexad.php">CASF</a>
         <!-- Navbar Search Input-->
         <!-- * * Note: * * Visible only on and above the lg breakpoint-->
         <!--
--- Admin/IncludesPro/header.php
+++ Admin/IncludesPro/header.php
@@ -49,7 +49,7 @@ $foto_src = $foto ? 'data:image/jpeg;base64,' . base64_encode($foto) : '/looneyt
         <!-- * * Tip * * You can use text or an image for your navbar brand.-->
         <!-- * * * * * * When using an image, we recommend the SVG format.-->
         <!-- * * * * * * Dimensions: Maximum height: 32px, maximum width: 240px-->
-        <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="/looneytunes/index.php">Dashboard</a>
+        <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="/looneytunes/index.php">CASF</a>
         <!-- Navbar Search Input-->
         <!-- * * Note: * * Visible only on and above the lg breakpoint-->
         <!--
